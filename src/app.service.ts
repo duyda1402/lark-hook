9700c1691009a0bdd5fb18ab92362704c4aa241e
Đỗ Duy <anhduycnttx1@gmail.com>
2024-02-01 15:56:06 +0700

    base

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Logger } from '@nestjs/common';
+import { LarkEncryptVerificationRequestDto } from './dto/verification-request.dto';
+import { AESCipher } from './lark-event-decryption';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  logger = new Logger(AppService.name);
+  private readonly LARK_API_URL = 'https://open.larksuite.com';
+  private LARK_APP_ID = 'cli_a531f91211f8d010';
+  private LARK_ENCRYPT_KEY = 'kFDK9QJCCIjwvgp0RZsJNvSE2WUwQzui';
+
+  constructor() {}
+
+  async eventWebhook(request: LarkEncryptVerificationRequestDto): Promise<any> {
+    try {
+      this.logger.log(`eventWebhook request = ${JSON.stringify(request)}`);
+
+      const cipher = new AESCipher(this.LARK_ENCRYPT_KEY);
+      const challengeDecrypt = cipher.decrypt(request.encrypt);
+      this.logger.log(`eventWebhook challengeDecrypt = ${challengeDecrypt}`);
+      const verifyObj = JSON.parse(challengeDecrypt);
+
+      // return challenge to verify webhook url
+      if (
+        verifyObj.hasOwnProperty('type') &&
+        verifyObj.type === 'url_verification'
+      ) {
+        return {
+          challenge: verifyObj.challenge,
+        };
+      }
+
+      const messageId = verifyObj?.event?.message?.message_id;
+
+      const messageContent = verifyObj?.event?.message?.content || '';
+      const messageContentObj = JSON.parse(messageContent);
+      console.log(messageId, messageContentObj);
+    } catch (error) {
+      this.logger.error(`eventWebhook get error = ${error.stack}`);
+    }
   }
 }
