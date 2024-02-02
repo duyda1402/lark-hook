a7eeb5c3e3cb5b49bdc0a64408a1faa600be45af
Đỗ Duy <anhduycnttx1@gmail.com>
2024-02-02 14:38:54 +0700

    add upload status

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,27 +1,26 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { LarkEncryptVerificationRequestDto } from './dto/verification-request.dto';
 import { AESCipher } from './lark-event-decryption';
+import axios from 'axios';
 @Injectable()
 export class AppService {
   logger = new Logger(AppService.name);
   private readonly LARK_API_URL = 'https://open.larksuite.com';
   private LARK_APP_ID = 'cli_a531f91211f8d010';
   private LARK_ENCRYPT_KEY = '8W2Zgbw3WpCRdJawciwfFcAglvFiK1PU';
-
+  private nFLOW_URL_UPLOAD =
+    'https://banca_predev2.nflow.staging.nuclent.com/v1/f/apiApprovalRequestSystem';
   constructor() {}
 
   async eventWebhook(request: LarkEncryptVerificationRequestDto): Promise<any> {
     try {
-      console.log('===========');
-      this.logger.log(
-        `eventWebhook request = ${JSON.stringify(request?.encrypt)}`,
-      );
+      this.logger.log(`Encrypt Event Request : ${request?.encrypt} ==========`);
 
       const cipher = new AESCipher(this.LARK_ENCRYPT_KEY);
       const challengeDecrypt = cipher.decrypt(request.encrypt);
-      //this.logger.log(`eventWebhook challengeDecrypt = ${challengeDecrypt}`);
       const verifyObj = JSON.parse(challengeDecrypt);
-      console.log('verifyObj', verifyObj);
+      console.log('Object Decode', verifyObj);
+      //Verify hash encrypt for lark
       if (
         verifyObj.hasOwnProperty('type') &&
         verifyObj.type === 'url_verification'
@@ -30,11 +29,32 @@ export class AppService {
           challenge: verifyObj.challenge,
         };
       }
-      // const eventId = verifyObj?.uuid;
-      // const eventContent = verifyObj?.event;
-      // console.log(eventId, eventContent);
+      const typeEvent = verifyObj?.event?.type;
+      if (typeEvent === 'approval_instance') {
+        const status = verifyObj?.event?.status;
+        const instanceCode = verifyObj?.event?.instance_code;
+        await this.uploadStatusRequest(instanceCode, status);
+      }
     } catch (error) {
       this.logger.error(`eventWebhook get error = ${error.stack}`);
+    }
+  }
+
+  async uploadStatusRequest(instanceCode: string, status: string) {
+    try {
+      const res = await axios.post(this.nFLOW_URL_UPLOAD, {
+        instanceCode,
+        status,
+      });
+      if (res.data.statusCode !== 200) {
+        this.logger.error('Error Uploading Status: ' + res.data.message);
+      } else {
+        this.logger.log(
+          `Upload Status with instanceCode: ${instanceCode} successfully!`,
+        );
+      }
+    } catch (error: any) {
+      this.logger.error('Error Uploading Status: ' + error.message);
     }
   }
 }
