f902b6dc3e51252f6f174da0dbe639618d20626e
Duy Do <43140987+duyda1402@users.noreply.github.com>
2024-02-02 07:05:08 +0000

    Add config

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,20 +6,22 @@ export class AppService {
   logger = new Logger(AppService.name);
   private readonly LARK_API_URL = 'https://open.larksuite.com';
   private LARK_APP_ID = 'cli_a531f91211f8d010';
-  private LARK_ENCRYPT_KEY = 'kFDK9QJCCIjwvgp0RZsJNvSE2WUwQzui';
+  private LARK_ENCRYPT_KEY = '8W2Zgbw3WpCRdJawciwfFcAglvFiK1PU';
 
   constructor() {}
 
   async eventWebhook(request: LarkEncryptVerificationRequestDto): Promise<any> {
     try {
-      this.logger.log(`eventWebhook request = ${JSON.stringify(request)}`);
+      console.log('===========');
+      this.logger.log(
+        `eventWebhook request = ${JSON.stringify(request?.encrypt)}`,
+      );
 
       const cipher = new AESCipher(this.LARK_ENCRYPT_KEY);
       const challengeDecrypt = cipher.decrypt(request.encrypt);
-      this.logger.log(`eventWebhook challengeDecrypt = ${challengeDecrypt}`);
+      //this.logger.log(`eventWebhook challengeDecrypt = ${challengeDecrypt}`);
       const verifyObj = JSON.parse(challengeDecrypt);
-
-      // return challenge to verify webhook url
+      console.log('verifyObj', verifyObj);
       if (
         verifyObj.hasOwnProperty('type') &&
         verifyObj.type === 'url_verification'
@@ -28,10 +30,9 @@ export class AppService {
           challenge: verifyObj.challenge,
         };
       }
-
-      const eventId = verifyObj?.uuid;
-      const eventContent = verifyObj?.event;
-      console.log(eventId, eventContent);
+      // const eventId = verifyObj?.uuid;
+      // const eventContent = verifyObj?.event;
+      // console.log(eventId, eventContent);
     } catch (error) {
       this.logger.error(`eventWebhook get error = ${error.stack}`);
     }
